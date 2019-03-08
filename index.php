e56355344e28f78d7672cf0c5ccb12fe76045ddb
petardinic <petardinic93@gmail.com>
2019-03-08 14:52:50 +0100

    delte comment

--- index.php
+++ index.php
@@ -6,22 +6,3 @@ include_once ('config/config.php');
 $action = new ArticlesController();
 
 $action->pageAction($config);
-
-
-
-//include_once 'config.php';
-//
-//if(file_exists(__DIR__."/views/" . $config['task'] . ".html.php"))
-//{
-//    include_once "views/" . $config['task'] . ".html.php";
-//}
-//else 
-//{
-//    $config['error'] = "Requeste View page views/" . $config['task'] . ".html.php do not exists!";
-//    
-//    include_once "views/error.html.php";
-//}
-//
-
-
-
